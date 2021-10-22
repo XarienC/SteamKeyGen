c65c9219d847f7ec7ee61270fffb90a55a6087ad
Matt <50338960+Matt101-code@users.noreply.github.com>
2021-10-22 15:30:13 +0100

    Add project files.

---
+++ SteamKeyGen/KeyGen.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace SteamKeyGen
+{
+    class KeyGen
+    {
+
+        private string KeyLetters;
+        private string KeyNumbers;
+        private int KeyChars;
+        private char[] LetterArray;
+        private char[] NumberArray;
+
+
+        protected internal string KeyLetters_
+        {
+            set
+            {
+                KeyLetters = value;
+            }
+        }
+
+        protected internal string KeyNumbers_
+        {
+            set
+            {
+                KeyNumbers = value;
+            }
+        }
+
+        protected internal int KeyChars_
+        {
+            set
+            {
+                KeyChars = value;
+            }
+        }
+
+        public string GenerateKey()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            LetterArray = KeyLetters.ToCharArray();
+            NumberArray = KeyNumbers.ToCharArray();
+            int keyChars = KeyChars;
+            checked
+            {
+                for (int i = 1; i <= keyChars; i++)
+                {
+                    VBMath.Randomize();
+                    float num1 = VBMath.Rnd();
+                    short num2 = -1;
+                    bool flag = (int)Math.Round((double)(unchecked(num1 + 111f))) % 2 == 0;
+                    if (flag)
+                    {
+                        while (num2 < 0)
+                        {
+                            num2 = Convert.ToInt16(unchecked((float)LetterArray.GetUpperBound(0) * num1));
+                        }
+                        string text = Conversions.ToString(LetterArray[(int)num2]);
+                        bool flag2 = (int)Math.Round((double)(unchecked((float)num2 * num1 * 99f))) % 2 != 0;
+                        if (flag2)
+                        {
+                            text = this.LetterArray[(int)num2].ToString();
+                            text = text.ToUpper();
+                        }
+                        stringBuilder.Append(text);
+                    }
+                    else
+                    {
+                        while (num2 < 0)
+                        {
+                            num2 = Convert.ToInt16(unchecked((float)this.NumberArray.GetUpperBound(0) * num1));
+                        }
+                        stringBuilder.Append(this.NumberArray[(int)num2]);
+                    }
+                }
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
---
+++ SteamKeyGen/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Diagnostics;
+
+namespace SteamKeyGen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            NoteMSG.Content = "Note for users:\nThis does not generate working Steam keys.\n\nThis tool generates keys in the Steam key format\nthat can be used for checking.\n\nExample: P2R4S-TVR2S-30YR1\n\n\n\n\n\nMade by cracked.to/Matt101";
+        }
+
+        private void SelectOutPutLocation_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.Description = "Choose output location:";
+            dialog.ShowDialog();
+            LocationOutput.Text = dialog.SelectedPath;
+        }
+
+        private void GenKeysButton_Click(object sender, RoutedEventArgs e)
+        {
+            KeyGen key = new KeyGen();
+            key.KeyLetters_ = "ABCDEFGHIJKLMNOPQRSTUVWYZ";
+            key.KeyNumbers_ = "0123456789";
+            key.KeyChars_ = 5;
+            int num = Conversions.ToInteger(KeyAmount.Text);
+            string folderpath = LocationOutput.Text;
+            FileStream stream = new FileStream(folderpath + "\\" + Conversions.ToString(num) + " Steam Keys.txt", FileMode.Create);
+            TextWriter @out = Console.Out;
+            StreamWriter streamWriter = new StreamWriter(stream);
+            Console.SetOut(streamWriter);
+            int num2 = num;
+            checked
+            {
+                for (int i = 1; i <= num2; i++)
+                {
+                    string text = key.GenerateKey();
+                    Console.WriteLine(string.Concat(new string[]
+                    {
+                        key.GenerateKey(),
+                        "-",
+                        key.GenerateKey(),
+                        "-",
+                        key.GenerateKey()
+                    }));
+                }
+                Console.SetOut(@out);
+                streamWriter.Close();
+                System.Windows.MessageBox.Show("Generated " + Conversions.ToString(num) + " keys");
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void MattEllipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("https://cracked.to/Matt101");
+        }
+    }
+}
